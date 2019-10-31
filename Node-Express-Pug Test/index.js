186f27cc3ac38605debf38250efb7b926483655c
olivyama11 <olivyama1149@gmail.com>
2019-10-31 13:43:05 -0700

    create first main view and add stylesheet

--- Node-Express-Pug Test/index.js
+++ Node-Express-Pug Test/index.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const mysql = require('mysql');
 const app = express();
+var path = require('path');
 
 function getConnection() {
 	return mysql.createConnection({
 		host	: 'localhost',
 		user	: 'root',
-		password: 'Something0clever.',
+		password: 'root',
 		database: 'cs157a'
 	});
 }
 
 app.set('view engine', 'pug');
 app.set('views', './views');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
 	res.render('first_view');
@@ -24,14 +26,14 @@ app.get('/demo', function(req, res) {
 	var connection = getConnection();
 	connection.connect();
 
-	connection.query('select * from emp', function(err, rows, fields) {
+	connection.query('select * from users', function(err, rows, fields) {
 		if (err) { throw err; }
 		else {
 			for (var i = 0; i < rows.length; i++) {
 				var person = {
-					'id' : rows[i].id,
-					'name' : rows[i].name,
-					'age' : rows[i].age
+					'id' : rows[i].userID,
+					'name' : rows[i].username,
+					'pw' : rows[i].password
 				}
 				list.push(person);
 			}
