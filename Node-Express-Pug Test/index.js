a1595144ea52fa0fdd792c3f5c1775819885b2b0
olivyama11 <olivyama1149@gmail.com>
2019-11-11 21:35:36 -0800

    add login and register part

--- Node-Express-Pug Test/index.js
+++ Node-Express-Pug Test/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const mysql = require('mysql');
 const app = express();
-var path = require('path');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const path = require('path');
+
 
 function getConnection() {
 	return mysql.createConnection({
@@ -15,10 +18,92 @@ function getConnection() {
 app.set('view engine', 'pug');
 app.set('views', './views');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true,
+    cookie: {
+        maxAge: 60 * 1000 * 30
+    },
+    loggedin: false
+}));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
 
 app.get('/', function(req, res){
-	res.render('first_view');
+	if (req.session.loggedin) {
+		res.redirect('/home');
+	} else {
+		res.render('login');
+	}
 });
+
+app.get('/home', function(req, res) {
+    if (req.session.loggedin) {
+        res.render('first_view');
+        return;
+    }
+    res.render('/');
+})
+app.post('/auth', function(req, res){
+	var username = req.body.username;
+	var password = req.body.password;
+	var connection = getConnection();
+	connection.connect();
+	if (username && password) {
+		connection.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			if (results.length > 0) {
+				req.session.loggedin = true;
+				req.session.username = username;
+				res.redirect('/');
+			} else {
+				res.send('Incorrect Username and/or Password!');
+			}
+			res.end();
+		});
+	} else {
+		res.send('Please enter Username and Password!');
+		res.end();
+	}
+	connection.end();
+});
+
+app.get('/register', function (req, res) {
+    res.render('register');
+
+});
+
+
+app.post('/register', function(req, res){
+	var username = req.body.username;
+	var password = req.body.password;
+	var email = req.body.email;
+	// console.log(email);
+    var connection = getConnection();
+    connection.connect();
+    if (username === "" || password == "") {
+    	res.send('Please fill in all fields');
+		res.end();
+	}
+    if (username.match(/^[a-z0-9_]+$/) === null) {
+    	res.send('Invalid username');
+    	res.end();
+	}
+    // password = md5(username + md5(password));
+	connection.query('INSERT INTO users VALUES (?,?,?,?)', [username, username, password, email], function(error, results, fields) {
+        var status;
+		if (error) {status = "user exists"}
+        else {
+        	status = "successfully register";
+		}
+		res.render('register', {"status" : status});
+        // res.end();
+	});
+    connection.end();
+
+});
+
 
 app.get('/demo', function(req, res) {
 	var list = [];
@@ -44,3 +129,4 @@ app.get('/demo', function(req, res) {
 })
 
 app.listen(3000);
+module.exports = app;
