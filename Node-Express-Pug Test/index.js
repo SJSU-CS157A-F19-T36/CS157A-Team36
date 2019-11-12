3de5ec1aa6e9826b5d5fa5d953e0460eb53b8eda
olivyama11 <olivyama1149@gmail.com>
2019-11-11 23:58:48 -0800

    add logout and show recipes

--- Node-Express-Pug Test/index.js
+++ Node-Express-Pug Test/index.js
@@ -22,9 +22,9 @@ app.use(session({
 	secret: 'secret',
 	resave: true,
 	saveUninitialized: true,
-    cookie: {
-        maxAge: 60 * 1000 * 30
-    },
+    // cookie: {
+    //     maxAge: 60 * 1000 * 30
+    // },
     loggedin: false
 }));
 app.use(bodyParser.urlencoded({extended : true}));
@@ -41,10 +41,31 @@ app.get('/', function(req, res){
 
 app.get('/home', function(req, res) {
     if (req.session.loggedin) {
-        res.render('first_view');
-        return;
-    }
-    res.render('/');
+		var username = req.session.loggedin;
+		var recipe_list = [];
+		var connection = getConnection();
+		connection.connect();
+		connection.query('SELECT * FROM recipes', null, function (err, results, fields) {
+			if (err) { throw err; }
+			else {
+				for (var i = 0; i < results.length; i++) {
+					var recipe = {
+						'name' : results[i].recipeID,
+						'image' : results[i].image_url
+					};
+					recipe_list.push(recipe);
+				}
+				console.log(recipe_list[0].name);
+				console.log(recipe_list[0].image);
+				res.render('first_view', {"user" : username , "recipes" : recipe_list});
+
+			}
+		})
+		connection.end();
+    } else {
+		res.redirect('/');
+	}
+
 })
 app.post('/auth', function(req, res){
 	var username = req.body.username;
@@ -54,8 +75,8 @@ app.post('/auth', function(req, res){
 	if (username && password) {
 		connection.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
 			if (results.length > 0) {
-				req.session.loggedin = true;
-				req.session.username = username;
+				req.session.loggedin = username;
+				// req.session.username = username;
 				res.redirect('/');
 			} else {
 				res.send('Incorrect Username and/or Password!');
@@ -104,6 +125,24 @@ app.post('/register', function(req, res){
 
 });
 
+app.get('/logout', function(req, res) {
+	req.session.loggedin = false;
+	res.redirect('/');
+})
+
+
+/* NEED DETAIL PAGE
+app.get('/detail', function(req, res) {
+	var name = req.query.name;
+	console.log(name);
+	var connection = getConnection();
+	connection.connect();
+	connection.query('SELECT * FROM recipes', function(err, rows, fields) {
+
+	});
+});
+
+**/
 
 app.get('/demo', function(req, res) {
 	var list = [];
@@ -111,22 +150,22 @@ app.get('/demo', function(req, res) {
 	var connection = getConnection();
 	connection.connect();
 
-	connection.query('select * from users', function(err, rows, fields) {
+	connection.query('SELECT * FROM recipes', function(err, rows, fields) {
 		if (err) { throw err; }
 		else {
 			for (var i = 0; i < rows.length; i++) {
 				var person = {
-					'id' : rows[i].userID,
-					'name' : rows[i].username,
-					'pw' : rows[i].password
-				}
+					'id' : rows[i].recipeID,
+					'name' : rows[i].image_url
+					// 'pw' : rows[i].password
+				};
 				list.push(person);
 			}
 			res.render('demo', {"list" : list});
 		}
-	})
+	});
 	connection.end();
-})
+});
 
 app.listen(3000);
 module.exports = app;
